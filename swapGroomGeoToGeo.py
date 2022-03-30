eb610ebc16e23fa2819ce95ec3cca88f21e510bc
Fernando <nandoomat@gmail.com>
2022-03-30 13:15:57 -0300

    swapGroomGeoToGeo
    swapGroomGeoToGeo

---
+++ swapGroomGeoToGeo.py
@@ -0,0 +1,76 @@
+import pymel.core as pmc
+import maya.cmds as cmds
+import sys
+
+def validateObj(selectList):
+    message = []
+    if len(selectList) !=2 or selectList == None :
+        pmc.warning("Select at least 2 objects to link")
+        return False
+
+    for item in selectList:
+        if pmc.nodeType(item) == "transform":
+            if pmc.listRelatives(item, shapes=True) == None:
+                pmc.warning("No shape found on: " + item)
+                message.append(False)
+            else:
+                message.append(True)
+        else:
+            pmc.warning("Please select a transform node")
+            message.append(False)
+    print(str(message[0]) + " | " + str(message[1]))
+    return message[0] and message[1]
+
+def organizeObj(selectList):
+    for item in selectList:
+        item = str(pmc.listRelatives(item,s=True)[0])
+        geoReference = cmds.connectionInfo(item +'.referenceObject',sourceFromDestination=True)
+        if geoReference != '':
+            originalShape = item
+            geoShape_reference = str(geoReference.split('.')[0])
+            pmc.setAttr(geoShape_reference+".yetiSubdivision",1)
+        else:
+            geoShape = item
+    return originalShape, geoShape, geoShape_reference
+
+def returnGeoShapeByTypeConnection(shape,typeConnection):
+    connectedShape = cmds.listConnections(shape + '.' + typeConnection, sh=True)
+    return connectedShape
+
+def connectObj(geoShape_reference,geoShape):
+    pmc.connectAttr( geoShape_reference + ".message", geoShape + ".referenceObject", f=True)
+    return cmds.isConnected(geoShape_reference + ".message", geoShape + ".referenceObject")
+
+def disconnectObj(geoShape_reference,geoShape):
+    if cmds.isConnected(geoShape_reference + ".message", geoShape + ".referenceObject") == True:
+        pmc.disconnectAttr( geoShape_reference + ".message", geoShape + ".referenceObject")
+        return cmds.isConnected(geoShape_reference + ".message", geoShape + ".referenceObject")
+
+def swapGeometryOnGroom(originalShape,geoShape):
+    pmc.mel.eval('pgYetiSwapGeometry("'+ originalShape +'","'+ geoShape +'")')
+
+        
+def swapGeometryOnGeoCollision(originalShape,geoShape):
+    listCollisionGRM = cmds.connectionInfo(originalShape + ".worldMesh",dfs=True)
+    if len(listCollisionGRM) != 0:
+        for CollisionGRM in listCollisionGRM:
+            pmc.disconnectAttr( originalShape + ".worldMesh", CollisionGRM,)
+            pmc.connectAttr( geoShape + ".worldMesh" , CollisionGRM, f=True)
+            
+
+selectList = cmds.ls(sl=1, sn=True)
+
+if validateObj(selectList):
+    originalShape, geoShape, geoShape_reference = organizeObj(selectList)
+    if not disconnectObj(geoShape_reference,originalShape):
+        pmc.warning("Disconnected: " + originalShape)
+        if connectObj(geoShape_reference,geoShape):
+            pmc.warning("Connection successful")
+            swapGeometryOnGeoCollision(originalShape,geoShape)
+            swapGeometryOnGroom(originalShape ,geoShape)
+        else:
+            pmc.warning("Connection failed")
+    else:
+        pmc.warning("Disconnection failed")
+else:
+    pmc.warning("Try again by selecting 2 objects")
